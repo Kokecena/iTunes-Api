655eaebe943262f2b3ad498e2365a41890a2f1fa
Kokecena <jovanni_n@live.com.mx>
2024-03-28 14:57:06 -0600

    Se cambio el nombre de la clase Result a Response para ser mas acorde a que es una respuesta de la API

--- src/main/java/com/github/kokecena/itunes/service/ITunesService.java
+++ src/main/java/com/github/kokecena/itunes/service/ITunesService.java
@@ -1,7 +1,7 @@
 package com.github.kokecena.itunes.service;
 
 import com.github.kokecena.itunes.keys.KeyParameters;
-import com.github.kokecena.itunes.models.Result;
+import com.github.kokecena.itunes.models.Response;
 import reactor.core.publisher.Mono;
 import retrofit2.http.GET;
 import retrofit2.http.QueryMap;
@@ -17,8 +17,8 @@ public interface ITunesService {
      * Searches for content in the iTunes Store based on the specified query parameters.
      *
      * @param query A map containing query parameters for the search. Use {@link KeyParameters} enum for keys.
-     * @return A Mono emitting a {@link Result} object representing the search results.
+     * @return A Mono emitting a {@link Response} object representing the search results.
      */
     @GET("search")
-    Mono<Result> search(@QueryMap Map<String, String> query);
+    Mono<Response> search(@QueryMap Map<String, String> query);
 }
